a5d71a12a08b85e1fe14663f77093ff6eefac875
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2023-05-17 18:19:42 +0300

    Swaggerda Test İşlemi

--- MyUdemyProject/ApiConsume/HotelProject.WebApi/Controllers/StaffController.cs
+++ MyUdemyProject/ApiConsume/HotelProject.WebApi/Controllers/StaffController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelProject.WebApi.Controllers
@@ -7,15 +9,26 @@ namespace HotelProject.WebApi.Controllers
     [ApiController]
     public class StaffController : ControllerBase
     {
+        private readonly IStaffService _staffService;
+
+        public StaffController(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
+
         [HttpGet]
         public IActionResult StaffList()
         {
-            return Ok();
+           var values = _staffService.TGetList();
+            return Ok(values);
+
         }
         [HttpPost]
-        public IActionResult AddStaff()
+        public IActionResult AddStaff(Staff staff)
         {
+            _staffService.TInsert(staff);
             return Ok();
+
         }
         [HttpDelete]
         public IActionResult DeleteStaff()
