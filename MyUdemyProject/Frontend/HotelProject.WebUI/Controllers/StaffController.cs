e2eec67509db3eb66770e8e894851b0d4faf4d8c
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2023-05-18 06:40:00 +0300

    Apinin Consume Edilmesi

--- MyUdemyProject/Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ MyUdemyProject/Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class StaffController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public StaffController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:6027/api/Staff");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
